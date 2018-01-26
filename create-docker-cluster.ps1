2205ce2197449462d58a4f18cacde4d78d807968
Eric Collazo <ericcollazo67@gmail.com>
2018-01-26 16:45:24 -0500

    Fixed spacing.

--- create-docker-cluster.ps1
+++ create-docker-cluster.ps1
@@ -8,8 +8,10 @@ $apiToken = $values.do_api_token
 # manager-0
 docker-machine create --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc3" --digitalocean-size "2gb" --digitalocean-access-token $apiToken manager-0
 $manager0ip = docker-machine ip manager-0
+
 # Initiate swarm
 docker-machine ssh manager-0 docker swarm init --advertise-addr $manager0ip
+
 # Get join tokens
 $managerJoinToken = docker-machine ssh manager-0 docker swarm join-token manager -q
 $workerJoinToken = docker-machine ssh manager-0 docker swarm join-token worker -q
@@ -18,6 +20,7 @@ $joinIp = $manager0ip + ":" + $swarmPort
 $ucpUsr = "admin"
 $ucpPwd = "adminadmin"
 $ucpPort = "2378"
+
 # Install UCP
 docker-machine ssh manager-0 docker container run --rm -it --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.2.4 install --host-address $manager0ip --admin-username $ucpUsr --admin-password $ucpPwd --swarm-port $ucpPort
 
