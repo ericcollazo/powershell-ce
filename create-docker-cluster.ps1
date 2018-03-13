2cf34c3287d90d7fe1e9d220985cd87a069e6dd0
Eric Collazo <ericcollazo67@gmail.com>
2018-03-13 00:51:33 -0400

    Updated versions of UCP & DTR.

--- create-docker-cluster.ps1
+++ create-docker-cluster.ps1
@@ -23,7 +23,7 @@ $ucpPwd = "adminadmin"
 $ucpPort = "2378"
 
 # Install UCP
-docker-machine ssh manager-0 docker container run --rm -it --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.2.4 install --host-address $manager0ip --admin-username $ucpUsr --admin-password $ucpPwd --swarm-port $ucpPort
+docker-machine ssh manager-0 docker container run --rm -it --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.2.5 install --host-address $manager0ip --admin-username $ucpUsr --admin-password $ucpPwd --swarm-port $ucpPort
 
 # worker-0
 docker-machine create  --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc3" --digitalocean-size "2gb" --digitalocean-access-token $apiToken worker-0
@@ -35,7 +35,7 @@ docker-machine ssh dtr-0 docker swarm join --token $workerJoinToken $joinIp
 docker node update --availability drain dtr-0
 
 # Install DTR
-docker-machine ssh dtr-0 docker pull docker/dtr:2.4.1
+docker-machine ssh dtr-0 docker pull docker/dtr:2.4.2
 docker-machine ssh dtr-0 docker run -it --rm docker/dtr:2.4.1 install --ucp-node dtr-0 --ucp-url https://$manager0ip --ucp-username admin --ucp-password adminadmin --ucp-insecure-tls
 
 # List nodes in swarm
