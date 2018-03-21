47b2643fd1433010a2a1a5921a4ebbb9f8eb09b0
Eric Collazo <ericcollazo67@gmail.com>
2018-03-21 16:45:24 -0400

    Changed region.

--- create-docker-cluster.ps1
+++ create-docker-cluster.ps1
@@ -5,7 +5,7 @@ $values = Get-Content $Path | Out-String | ConvertFrom-StringData
 $apiToken = $values.do_api_token
 
 # manager-0
-docker-machine create --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc3" --digitalocean-size "8gb" --digitalocean-access-token $apiToken manager-0
+docker-machine create --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc1" --digitalocean-size "8gb" --digitalocean-access-token $apiToken manager-0
 $manager0ip = docker-machine ip manager-0
 docker-machine ssh manager-0 docker swarm init --advertise-addr $manager0ip
 
@@ -21,7 +21,7 @@ $ucpPort = "2378"
 docker-machine ssh manager-0 docker container run --rm -it --name ucp -v /var/run/docker.sock:/var/run/docker.sock docker/ucp:2.2.5 install --host-address $manager0ip --admin-username $ucpUsr --admin-password $ucpPwd --swarm-port $ucpPort
 
 # dtr-0
-docker-machine create --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc3" --digitalocean-size "8gb" --digitalocean-access-token $apiToken dtr-0
+docker-machine create --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc1" --digitalocean-size "8gb" --digitalocean-access-token $apiToken dtr-0
 docker-machine ssh dtr-0 docker swarm join --token $workerJoinToken $joinIp
 
 # Install DTR
@@ -30,15 +30,15 @@ docker-machine ssh dtr-0 docker pull docker/dtr:2.4.2
 docker-machine ssh dtr-0 docker run -it --rm docker/dtr:2.4.2 install --replica-id $dtrId --ucp-node dtr-0 --ucp-url https://$manager0ip --ucp-username admin --ucp-password adminadmin --ucp-insecure-tls
 
 # worker-0
-docker-machine create  --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc3" --digitalocean-size "4gb" --digitalocean-access-token $apiToken worker-0
+docker-machine create  --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc1" --digitalocean-size "4gb" --digitalocean-access-token $apiToken worker-0
 docker-machine ssh worker-0 docker swarm join --token $workerJoinToken $joinIp
 
 # worker-1
-docker-machine create  --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc3" --digitalocean-size "4gb" --digitalocean-access-token $apiToken worker-1
+docker-machine create  --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc1" --digitalocean-size "4gb" --digitalocean-access-token $apiToken worker-1
 docker-machine ssh worker-1 docker swarm join --token $workerJoinToken $joinIp
 
 # worker-2
-docker-machine create  --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc3" --digitalocean-size "4gb" --digitalocean-access-token $apiToken worker-2
+docker-machine create  --driver digitalocean --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "nyc1" --digitalocean-size "4gb" --digitalocean-access-token $apiToken worker-2
 docker-machine ssh worker-2 docker swarm join --token $workerJoinToken $joinIp
 
 # List nodes in swarm
