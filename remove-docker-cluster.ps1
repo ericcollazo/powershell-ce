ffc32af3492462758d5230497650f1bea9a8b3f1
Eric Collazo <ericcollazo67@gmail.com>
2018-03-31 11:39:47 -0400

    Added Jenkins & Minio vm's.

--- remove-docker-cluster.ps1
+++ remove-docker-cluster.ps1
@@ -1,6 +1,8 @@
 docker-machine rm  manager-0 -f
 
 docker-machine rm  dtr-0 -f
+docker-machine rm  minio-0 -f
+docker-machine rm  jenkins-0 -f
 
 docker-machine rm  worker-0 -f
 docker-machine rm  worker-1 -f
